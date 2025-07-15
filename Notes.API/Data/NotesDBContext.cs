aaf81f209036539a1c9a95bde0646fe568dfe170
bibekpandey0521 <bibekpandey276@gmail.com>
2025-07-15 11:50:52 +0545

    inital commit

---
+++ Notes.API/Data/NotesDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.API.Models.Entities;
+
+namespace Notes.API.Data
+{
+    public class NotesDBContext :DbContext //it will inherit from DbContext
+    { 
+        public NotesDBContext(DbContextOptions options) : base(options) //Constructor it options to base Class
+        { 
+            //
+        }
+        //Way 
+        public  DbSet<Note> Notes { get; set; } //Notes Table 
+    }
+}
